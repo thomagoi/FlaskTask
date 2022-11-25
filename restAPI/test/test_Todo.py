1c8cbe61a949677edc7a4937b04ca5ba8455d46e
Petersen <thomas.petersen@adesso.de>
2022-11-25 12:01:14 +0100

    update requirements.txt

--- restAPI/test/test_Todo.py
+++ restAPI/test/test_Todo.py
@@ -8,16 +8,10 @@ data2 = {"title":"TestNoDesc", "completed":False}
 response = requests.get(BASE + "todos")
 print(response.json())
 
-new_todo = {"title": "POST-Todo", "description": "This todo was postet with a POST-Request", "completed":True}
+new_todo = {"title": "POST-Todo", "description": "This todo was posted with a POST-Request", "completed":True}
 
-response = requests.post(BASE + "todos/post",json=new_todo)
-print(response.json())
-
-response = requests.get(BASE + "todos")
-print(response.json())
-
-# response = requests.post(BASE + 'todos', json=data)
-# print(response.json())
+#response = requests.post(BASE + "todos/post",json=new_todo)
+#print(response.json())
 
 # response = requests.get(BASE + "todos")
 # print(response.json())
