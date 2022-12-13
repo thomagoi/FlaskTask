42f813048c3cc18ca3a56e14a8764b69f8bed3e9
Thomas Petersen <ThomasLiang@outlook.de>
2022-12-13 14:55:10 +0100

    added config file

--- restAPI/src/app.py
+++ restAPI/src/app.py
@@ -4,10 +4,21 @@ from flask_migrate import Migrate
 from flask_swagger import swagger 
 from flask import jsonify
 
+import routing 
+import yaml 
+
+with open("config.yml","r") as ymlfile:
+    config = yaml.safe_load(ymlfile)
+
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = config['app']['db_uri']
 
 db.init_app(app)
+
+""" render_as_batch makes a new copy of table with applied changes to work around the problem of altering
+    the table, fix for working with sqlite
+"""
+migrate = Migrate(app,db,render_as_batch=True)
 
 @app.route("/documentation")
 def documentation():
@@ -16,14 +27,8 @@ def documentation():
     swag['info']['title'] = "FlaskTask"
     return jsonify(swag)
 
-""" render_as_batch makes a new copy of table with applied changes to work around the problem of altering
-    the table, fix for working with sqlite
-"""
-migrate = Migrate(app,db,render_as_batch=True)
-
-import routing 
 app.register_blueprint(routing.router,url_prefix="/api")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True,port=config['app']['port'])
 
